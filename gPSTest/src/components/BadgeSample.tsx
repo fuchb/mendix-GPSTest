29113a7b8d291f3fa5bd9e0f4039cd116f2022ae
fuchb <15842635958@163.com>
2020-03-27 17:22:08 +0900

    no message

--- gPSTest/src/components/BadgeSample.tsx
+++ gPSTest/src/components/BadgeSample.tsx
@@ -1,13 +1,14 @@
 import { Component, ReactNode, createElement } from "react";
 import { Map, Marker } from 'react-amap';
-//import Geolocation from '@react-native-community/geolocation';
+import Geolocation from '@react-native-community/geolocation';
 
 export class BadgeSample extends Component {
     private position = { center:{longitude: 114.16936109999999 , latitude: 38}};
     constructor(props: any) {
         super(props);
         //this.state = { center:{longitude: 114.16936109999999 , latitude: 38}};
-        navigator.geolocation.getCurrentPosition(info => {
+        //navigator.geolocation.getCurrentPosition(info => {
+        Geolocation.getCurrentPosition(info => {
             this.position.center.latitude = info.coords.latitude;
             this.position.center.longitude = info.coords.longitude;
         });
